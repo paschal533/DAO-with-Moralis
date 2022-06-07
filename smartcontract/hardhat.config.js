2a041fbda4949ac2c013caeae2b1ffab500d1880
paschal533 <okwuosahpaschal@gmail.com>
2022-06-07 01:11:16 +0100

    Update commit

--- smartcontract/hardhat.config.js
+++ smartcontract/hardhat.config.js
@@ -1,5 +1,9 @@
 require("@nomiclabs/hardhat-waffle");
+require("@nomiclabs/hardhat-etherscan")
+const dotenv = require("dotenv")
 
+
+dotenv.config()
 // This is a sample Hardhat task. To learn how to create your own go to
 // https://hardhat.org/guides/create-task.html
 task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
@@ -18,4 +22,13 @@ task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
  */
 module.exports = {
   solidity: "0.8.4",
+  networks: {
+    mumbai: {
+      url: process.env.POLYGON_MUMBAI,
+      accounts: [process.env.PRIVATE_KEY]
+    },
+  },
+  etherscan: {
+    apiKey: process.env.API_KEY,
+  }
 };
