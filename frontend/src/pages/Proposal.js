69e7f1b9ad1159222717cac2530cdf4c4f4f8f69
paschal533 <okwuosahpaschal@gmail.com>
2022-06-10 21:53:58 +0100

    Update commit

--- frontend/src/pages/Proposal.js
+++ frontend/src/pages/Proposal.js
@@ -21,7 +21,7 @@ const Proposal = () => {
       
       async function getVotes() {
         
-        //Sconst Votes = Moralis.Object.extend("Votes");
+        const Votes = Moralis.Object.extend("Votes");
         const query = new Moralis.Query(Votes);
         query.equalTo("proposal", proposalDetails.id);
         query.descending("createdAt");
