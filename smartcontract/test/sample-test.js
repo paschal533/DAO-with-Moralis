1faae9d5561648ab0199c63078bcb5aaccd466e5
Paschal <58183764+paschal533@users.noreply.github.com>
2022-06-20 22:09:26 +0100

    Update simple-test.js

--- smartcontract/test/sample-test.js
+++ smartcontract/test/sample-test.js
@@ -4,7 +4,7 @@ const { ethers } = require("hardhat");
 describe("Greeter", function () {
   it("Should return the new greeting once it's changed", async function () {
     const Greeter = await ethers.getContractFactory("Greeter");
-    const greeter = await Greeter.deploy("Hello, world!");
+    //const greeter = await Greeter.deploy("Hello, world!");
     await greeter.deployed();
 
     expect(await greeter.greet()).to.equal("Hello, world!");
